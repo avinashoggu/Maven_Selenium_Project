36c5f2f61a20120d55fb11d593c7f782935f4b6d
Avinash_Oggu <Avinash_Oggu@192.168.56.1>
2018-01-25 14:22:19 +0530

    New Changes

--- src/test/java/com/TestBase/TestBase.java
+++ src/test/java/com/TestBase/TestBase.java
@@ -38,7 +38,7 @@ public class TestBase {
 			test.log(LogStatus.INFO, result.getName() + " test is started");
 		}
 	}
-//After Method
+//*****comment
 	@AfterMethod()
 	public void afterMethod(ITestResult result) {
 		getresult(result);
